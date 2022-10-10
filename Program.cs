f8de4b70ce4bd59718ca25f499d216325b4edaab
PomortsevaAnastasia <massage.anastasia@mail.ru>
2022-10-10 21:04:42 +0300

    создание проекта по первой задаче

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу,которая принимает на вход два числа , а на выходе определяет какое число больше, а какое меньше
+
+Console.Write ("Введите первое число: ");
+int num1=Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int num2=Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{ 
+    Console.WriteLine ($"{num1} - большее из представленных двух чисел");
+}
+else 
+{
+    Console.WriteLine ($"{num2} - большее из представленных двух чисел");
+}
---
+++ hm1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу,которая принимает на вход два числа , а на выходе определяет какое число больше, а какое меньше
+
+Console.Write("Введите первое число: ");
+int a =Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int b =Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{ 
+    Console.WriteLine ($"{a} - большее из представленных двух чисел");
+}
+else 
+{
+    Console.WriteLine ($"{b} - большее из представленных двух чисел");
+}
