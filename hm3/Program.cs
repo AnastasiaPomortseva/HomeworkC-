44174245d5c6a5a1fff9d762fcc11d6c49180307
PomortsevaAnastasia <massage.anastasia@mail.ru>
2022-10-12 14:28:54 +0300

    решение 3 задачи

--- hm3/Program.cs
+++ hm3/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Введите число: ");
+int count=Convert.ToInt32(Console.ReadLine());
+ if (count%2==0)
+ {
+    Console.WriteLine("данное число четное");
+ }
+else
+{
+    Console.WriteLine("данное число нечетное");
+}
