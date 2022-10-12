3fbed2522eee014310318a1ea035d0997bcbac04
PomortsevaAnastasia <massage.anastasia@mail.ru>
2022-10-12 15:50:33 +0300

    решение 4 задачи

--- hm4/Program.cs
+++ hm4/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.Write("ВВедите число N: ");
+int N=Convert.ToInt32(Console.ReadLine());
+int count=1;
+for (count=1; count<=N; count++)
+ { if (count%2==0)
+    Console.Write(count + ", ");
+  }
