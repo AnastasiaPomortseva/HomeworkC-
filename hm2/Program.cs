2b399035858fba8094e66c5865cc59bdaa5c574b
PomortsevaAnastasia <massage.anastasia@mail.ru>
2022-10-12 13:35:52 +0300

    решение по 2 задаче и создание проекта для решения 3 задачи

--- hm2/Program.cs
+++ hm2/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//например 2, 3, 7 -> 7, 44 5 78 -> 78, 22 3 9 -> 22;
+
+Console.Write ("Введите первое число: ");
+int a =Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int b =Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите третье число: ");
+int d =Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if(a>b)
+{
+    max=a;
+    }
+if (b>a)
+{
+    max=b;
+    }
+if(max>d)
+{
+    Console.WriteLine(max + " большее число");
+}
+
+
+if(d>=max)
+{ 
+    max=d;
+    Console.WriteLine(d + " большее число");
+}
